da362e97afc25e29a9a6b718a11fb209687f9ca2
Bravegar <mark.vazquez123@gmail.com>
2023-09-22 05:25:21 -0500

    Reworked CSS files, added Bootstrap for styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar';
+import Tracklist from './Tracklist';
 import React, {useState,useEffect} from 'react';
 
 
@@ -10,23 +10,15 @@ function App() {
   useEffect(()=>{
     console.log(searchTerm);
   },[searchTerm]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
         <SearchBar onSearch={setSearchTerm}/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Tracklist />
+        
+        
     </div>
   );
 }
--- src/Track.js
+++ src/Track.js
@@ -1,3 +1,13 @@
 import './Track.css';
 
+const Track = (props) => {
+
+    return (
+        <div className='Track'>
+            <span className="Title">{props.name}</span>
+            <br />
+            <span className="SongInfo">Artist | Album</span>
+        </div>
+    )
+}
 export default Track;
